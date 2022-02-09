da55ca7a806ed8e266baead46e2b4ccdfb11da1b
Arslanoov <rasularslanoov@gmail.com>
2022-02-09 17:35:28 +0500

    feat(client): Added register page

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from "@angular/core";
 import { AuthRoutingModule } from "./auth-routing.module";
 
 import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
 
 @NgModule({
   imports: [
     AuthRoutingModule
   ],
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ]
 })
 
